5810d85823c5dcbefe63a11edb83fd38b052fefb
ajcode007 <santhoshkrishna932@gmail.com>
2026-08-03 23:55:13 +0530

    Data ingestion & Data Transformation added

---
+++ src/config/config.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessing.pkl")
